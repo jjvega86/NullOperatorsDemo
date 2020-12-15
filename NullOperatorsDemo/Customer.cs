07c28da0af5883b0d07288d08be8a12ae20131a5
jjvega86 <jj.vega86@gmail.com>
2020-12-15 16:15:53 -0500

    Created Customer class and set skeleton. Added GenerateCustomerId method.

---
+++ NullOperatorsDemo/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullOperatorsDemo
+{
+    class Customer
+    {
+        private string firstName;
+        private string lastName;
+        private int? customerId;
+        private List<Customer> customers;
+
+        public string FirstName { get => firstName; }
+        public string LastName { get => lastName; }
+        public int? CustomerId { get => customerId; }
+
+        public Customer()
+        {
+            customers = new List<Customer>();
+        }
+
+        
+
+        public int GenerateCustomerId()
+        {
+            Random random = new Random();
+            int newCustomerId = random.Next(0, 1000000000);
+
+            foreach(Customer customer in customers)
+            {
+                if (customer.customerId == newCustomerId)
+                {
+                    GenerateCustomerId();
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return newCustomerId;
+
+        }
+    }
+}
