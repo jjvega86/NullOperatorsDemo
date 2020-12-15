7d20507deeef264ec48bc24febb988ae4f8e2fd7
jjvega86 <jj.vega86@gmail.com>
2020-12-15 16:52:47 -0500

    Added example of Null Coalescing to Program.cs

--- NullOperatorsDemo/Customer.cs
+++ NullOperatorsDemo/Customer.cs
@@ -12,8 +12,8 @@ namespace NullOperatorsDemo
         private string lastName;
         private int? mailingId;
 
-        public string FirstName { get => firstName; }
-        public string LastName { get => lastName; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
         public int? MailingId { get => mailingId; set => mailingId = value; }
 
         
--- NullOperatorsDemo/Program.cs
+++ NullOperatorsDemo/Program.cs
@@ -10,6 +10,18 @@ namespace NullOperatorsDemo
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer();
+            MailingList mailingList = new MailingList();
+
+            // Null Coalescing
+            // Checks to see if left side is null. If null, print what's on the right side.
+            //customer1.FirstName = "Ben";
+            Console.WriteLine(customer1.FirstName ?? "undefined");
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
